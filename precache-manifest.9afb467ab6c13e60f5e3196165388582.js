b97673fd6f4c54db627a3f1c1ebd12ce36e195ba
MichaelWilkens <mwilkens12@gmail.com>
2019-04-27 11:18:13 -0700

    Updates

---
+++ precache-manifest.9afb467ab6c13e60f5e3196165388582.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c11cc337efd581331f3eda80dd6a94c",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "afd2e4d4d2abaed25726",
+    "url": "/clicky-game/static/css/main.ec6eb9d2.chunk.css"
+  },
+  {
+    "revision": "a686e41154fd92225c54",
+    "url": "/clicky-game/static/js/2.5bfccd3c.chunk.js"
+  },
+  {
+    "revision": "afd2e4d4d2abaed25726",
+    "url": "/clicky-game/static/js/main.0d7576d9.chunk.js"
+  },
+  {
+    "revision": "265538ab0e4606d350e6",
+    "url": "/clicky-game/static/js/runtime~main.27874bd3.js"
+  }
+]);
